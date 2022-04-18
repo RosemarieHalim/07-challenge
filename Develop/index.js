7cb5f7a4a404733f78f38634ba6c0a5586a94870
Rosemarie <rosemarie.m.halim@gmail.com>
2022-04-17 21:50:20 -0400

    created array and functions, added link.js for github link

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,124 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs= require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown.js');
+const link = ('./utils/link.js')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+   {
+       type: 'input',
+       name: 'project',
+       message: 'What is your project title? (Required)',
+       validate: nameInput => {
+           if (nameInput) {
+           return true;
+           } else {
+           console.log('Please enter your project name!');
+           return false;
+           }
+       }
+   },
+   {
+       type: 'input',
+       name: 'description',
+       message: 'Please provide a description of project:'
+   },
+   {
+       type: 'input',
+       name: 'installation',
+       message: 'Please provide installation instructions:',
+   },
+   {
+       type: 'input',
+       name: 'usage',
+       message: 'What is the useage of the project?'
+   },
+   {
+       type: 'input',
+       name: 'contributions',
+       message: 'Who are the constributors of the project?'
+   },
+   {
+       type: 'input',
+       name: 'test',
+       message: 'Is there tests included?'
+   },
+   {
+       type: 'list',
+       name: 'license',
+       message: 'What license did you use for the project:',
+       choises: [
+           'Apache',
+           'Boost',
+           'CC0',
+           'Eclipse',
+           'IBM',
+           'MIT',
+           'Mozilla',
+           'SIL',
+           'Unlicense'
+       ]
+   },
+   {
+       type: 'input',
+       name: 'Username',
+       message: 'What is your GitHub Username? (Required)',
+       validate: nameInput => {
+           if (nameInput) {
+           return true;
+           } else {
+           console.log('Please enter your name!');
+           return false;
+           }
+       }
+   },
+   {
+       type: 'input',
+       name: 'email',
+        message: 'Please write your email. (Required)',
+       validate: nameInput => {
+           if (nameInput) {
+           return true;
+           } else {
+           console.log('Please enter your email!');
+           return false;
+           }
+       }
+   },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err)
+        }
+        console.log('README.md has been generated!')
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
+
+// Create a function to initialize app
+function init() {
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log('Your answers: ', userResponses);
+        console.log('Fetching GitHub data...')
+
+        const userInfo = await link.getUser(userResponses);
+        console.log('GitHub user info: ', userInfo);
+
+        console.log('Generating README.md...');
+        const md = markdown(userResponses, userInfo);
+
+        await writeFileAsync('ExampleREADME.md', md);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# Professional README Generator Starter Code
+#   Challenge 07 - Professional README Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+##  User Story
+
+AS A developer
+I WANT a README generator
+SO THAT I can quickly create a professional README for a new project
+
+## Acceptance Criteria
+
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for information about my application repository
+THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+WHEN I enter my project title
+THEN this is displayed as the title of the README
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
+
+---
+
+# My Portfolio
+
+## Purpose
+Quickly and easily generate a README file by using a command-line application to generate one. Allowing the project creator to devote more time working on the project.
+
+## Built With
+* HTML
+* CSS
+* Javascript
+* Node.js
+
+## Website
+https://rosemariehalim.github.io/07-challenge/
+
+---
+
+## Challenge 7.0
+
+* planned out code
+* added core code
+* 
